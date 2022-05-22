f1b6df6ac6a88c414010afa97e5ecef1e058cb9b
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2022-05-22 15:26:20 +0200

    fix update script with static repo url

--- install.lua
+++ install.lua
@@ -1,4 +1,3 @@
-local repoURL = "https://raw.githubusercontent.com/knijn/kav/main/"
-shell.run("wget " .. repoURL .. "startup/src/00_kav.lua /startup/00_kav.lua") -- Startup program for all the kav features, kav WILL NOT WORK if this fails
-shell.run("wget " .. repoURL .. ".pkg/src/kav.lua /.pkg/kav.lua") -- controller for kav, not strictly neccesery but very useful
-shell.run("wget " .. repoURL .. ".pkg/src/pastebin.lua /.pkg/pastebin.lua") -- patched pastebin with check
+shell.run("wget https://raw.githubusercontent.com/knijn/kav/main/src/startup/00_kav.lua /startup/00_kav.lua") -- Startup program for all the kav features, kav WILL NOT WORK if this fails
+shell.run("wget https://raw.githubusercontent.com/knijn/kav/main/src/.pkg/kav.lua /.pkg/kav.lua") -- controller for kav, not strictly neccesery but very useful
+shell.run("wget https://raw.githubusercontent.com/knijn/kav/main/src/.pkg/pastebin.lua /.pkg/pastebin.lua") -- patched pastebin with check
