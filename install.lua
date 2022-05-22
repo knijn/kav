32569627fd06970fe2872d0a3aa47b5d67c24c4d
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2022-05-22 15:31:48 +0200

    add reboot and settings add

--- install.lua
+++ install.lua
@@ -1,3 +1,8 @@
 shell.run("wget https://raw.githubusercontent.com/knijn/kav/main/src/startup/00_kav.lua /startup/00_kav.lua") -- Startup program for all the kav features, kav WILL NOT WORK if this fails
 shell.run("wget https://raw.githubusercontent.com/knijn/kav/main/src/.pkg/kav.lua /.pkg/kav.lua") -- controller for kav, not strictly neccesery but very useful
 shell.run("wget https://raw.githubusercontent.com/knijn/kav/main/src/.pkg/pastebin.lua /.pkg/pastebin.lua") -- patched pastebin with check
+shell.run("/startup/00_kav.lua")
+kav.reset()
+print("Rebooting!")
+sleep(1)
+os.reboot()
