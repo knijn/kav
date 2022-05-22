cc3fec8133816ef85ff376e4c739f3305b1ef4ba
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2022-05-22 14:51:08 +0200

    fix installed again

--- install.lua
+++ install.lua
@@ -1,4 +1,4 @@
-local repoURL = "https://raw.githubusercontent.com/knijn/kav/main"
+local repoURL = "https://raw.githubusercontent.com/knijn/kav/main/"
 shell.run("wget " .. repoURL .. "startup/00_kav.lua /startup/00_kav.lua") -- Startup program for all the kav features, kav WILL NOT WORK if this fails
 shell.run("wget " .. repoURL .. ".pkg/kav.lua /.pkg/kav.lua") -- controller for kav, not strictly neccesery but very useful
 shell.run("wget " .. repoURL .. ".pkg/pastebin.lua /.pkg/pastebin.lua") -- patched pastebin with check
