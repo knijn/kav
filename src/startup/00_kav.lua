5e9c9c57ff4f5e08f574944370aa1dbefa379151
Lupus590 <lupussolitarius590@gmail.com>
2022-05-23 13:22:05 +0100

    fix comment typos

--- src/startup/00_kav.lua
+++ src/startup/00_kav.lua
@@ -8,7 +8,7 @@ local kavServer = "https://raw.githubusercontent.com/knijn/kav/main"
 
 kav = {}
 kav.backendVersion = 1.0
-kav.advancedMenu = settings.get("kav.advancedMenu") or term.isColor() -- what someone is on an advansed computer and wants the normal prompt?
+kav.advancedMenu = settings.get("kav.advancedMenu") or term.isColor() -- what if someone is on an advanced computer and wants the normal prompt?
 
 local function warn(v)
   local oldTXT = term.getTextColor()
